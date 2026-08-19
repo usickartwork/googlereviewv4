6d7bbd967425a2b2cb0da621ac4067f454698af6
usickartwork <usick.artwork@gmail.com>
2026-08-19 21:08:20 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="id">
-      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
-        {children}
-      </body>
-    </html>
-  );
-}
